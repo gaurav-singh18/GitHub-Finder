4a9db20d9964617a63f4827e0354e4926e47df3f
Gaurav Singh <sgaurav4683@gmail.com>
2023-02-22 08:40:25 +0530

    home,about,notFound page added

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder </h1>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+}
+
+export default About;
